b8837e79003ae4c99447adee9957877a1ca95d5e
Stanley Wintergreen <kepocnhh@protonmail.com>
2025-08-15 23:29:14 +0300

    jdk-17.0.11-amd64:17d

--- amd64/jdk/17.0.11/push.sh
+++ amd64/jdk/17.0.11/push.sh
@@ -4,10 +4,10 @@ ARCH='amd64'
 PLATFORM="linux/${ARCH}"
 HOST='docker.io'
 NAMESPACE='kepocnhh'
-ISSUER='debian'
-ISSUER_VERSION='bullseye'
+ISSUER='jdk'
+ISSUER_VERSION='17.0.11'
 REPOSITORY="${ISSUER}-${ISSUER_VERSION}-${ARCH}"
-IMAGE_VERSION=8
+IMAGE_VERSION=17
 IMAGE_FLAVOR='d'
 IMAGE_TAG="${IMAGE_VERSION}${IMAGE_FLAVOR}"
 IMAGE_NAME="${HOST}/${NAMESPACE}/${REPOSITORY}:${IMAGE_TAG}"
@@ -30,40 +30,11 @@ docker run --platform="${PLATFORM}" \
 if test $? -ne 0; then
  echo 'Run error!'; exit 1; fi
 
-docker cp "${ARCH}/debian/bullseye/key.pgp" "${CONTAINER_NAME}:/tmp/key.pgp"
-if test $? -ne 0; then echo 'Copy error!'; exit 1; fi
-
 for it in \
  "test \"\$(cat /etc/flavor)\" == \"${IMAGE_FLAVOR}\"" \
- 'cat /etc/apt/sources.list' \
- 'curl --version' \
- 'openssl version' \
- 'zip --version' \
- 'yq --version' \
- 'echo "foo: bar" | yq -erM .foo' \
- 'git --version' \
- 'git clone https://github.com/kepocnhh/Dockerx.git' \
- 'git -C ./Dockerx status' \
- 'echo "foo" | openssl dgst -sha256 -binary | xxd -p -c 64' \
- 'cat ./Dockerx/README.md' \
- '/usr/local/bin/bash --version'; do
+ 'java --version'; do
  docker exec "${CONTAINER_NAME}" /usr/local/bin/bash -c "$it"
  if test $? -ne 0; then echo 'Exec error!'; exit 1; fi
-done
-
-for it in \
- 'gpg --version' \
- 'cat /usr/local/bin/gpgloopback.sh' \
- '/usr/local/bin/gpgloopback.sh --version; echo $?' \
- 'GPG_PASSWORD=foobarbaz /usr/local/bin/gpgloopback.sh --version' \
- 'gpg --batch --import /tmp/key.pgp' \
- 'gpg --list-keys' \
- 'gpg --list-secret-keys --keyid-format=long' \
- 'GPG_PASSWORD=foobarbaz /usr/local/bin/gpgloopback.sh --output /tmp/key.pgp.sig --detach-sig /tmp/key.pgp; echo $?' \
- 'GPG_PASSWORD=qwer1234 /usr/local/bin/gpgloopback.sh --output /tmp/key.pgp.sig --detach-sig /tmp/key.pgp' \
- 'gpg --verify /tmp/key.pgp.sig /tmp/key.pgp'; do
- docker exec "${CONTAINER_NAME}" /usr/local/bin/bash -c "$it"
- if test $? -ne 0; then echo 'GPG error!'; exit 1; fi
 done
 
 docker stop "${CONTAINER_NAME}"
