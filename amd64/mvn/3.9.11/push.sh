cdb7cb2a67dee830c064d6c9932f332458cb0091
Stanley Wintergreen <kepocnhh@protonmail.com>
2025-10-24 16:33:31 +0300

    mvn-3.9.11-amd64:4d

--- amd64/mvn/3.9.11/push.sh
+++ amd64/mvn/3.9.11/push.sh
@@ -8,7 +8,7 @@ NAMESPACE='kepocnhh'
 ISSUER='mvn'
 ISSUER_VERSION='3.9.11'
 REPOSITORY="${ISSUER}-${ISSUER_VERSION}-${ARCH}"
-IMAGE_VERSION=3
+IMAGE_VERSION=4
 IMAGE_FLAVOR='d'
 IMAGE_TAG="${IMAGE_VERSION}${IMAGE_FLAVOR}"
 IMAGE_NAME="${HOST}/${NAMESPACE}/${REPOSITORY}:${IMAGE_TAG}"
@@ -39,6 +39,68 @@ for it in \
  docker exec "${CONTAINER_NAME}" /usr/local/bin/bash -c "${it}"
  if test $? -ne 0; then echo 'Exec error!'; exit 1; fi
 done
+
+#
+
+REPOSITORY_OWNER='StanleyProjects'
+REPOSITORY_NAME='Useless.Java.Lib'
+
+WORK_DIR="/${REPOSITORY_OWNER}/${REPOSITORY_NAME}"
+
+docker exec "${CONTAINER_NAME}" mkdir -p "${WORK_DIR}"
+
+if test $? -ne 0; then
+ echo 'Make dir error!'; exit 1; fi
+
+SRC_COMMIT='6f7572406692f759bde01903a87f1fc165d8262f'
+
+for it in \
+ 'git init' \
+ "git remote add origin https://github.com/${REPOSITORY_OWNER}/${REPOSITORY_NAME}.git" \
+ "git fetch origin ${SRC_COMMIT}" \
+ "git checkout ${SRC_COMMIT}"; do
+ docker exec -w "${WORK_DIR}" "${CONTAINER_NAME}" /usr/local/bin/bash -c "${it}"
+ if test $? -ne 0; then echo 'Checkout error!'; exit 1; fi
+done
+
+for it in \
+ 'gradle clean' \
+ 'gradle sample:run'; do
+ docker exec -w "${WORK_DIR}" "${CONTAINER_NAME}" /usr/local/bin/bash -c "${it}"
+ if test $? -ne 0; then echo 'Gradle error!'; exit 1; fi
+done
+
+#
+
+REPOSITORY_OWNER='StanleyProjects'
+REPOSITORY_NAME='Useless.Android.Lib'
+
+WORK_DIR="/${REPOSITORY_OWNER}/${REPOSITORY_NAME}"
+
+docker exec "${CONTAINER_NAME}" mkdir -p "${WORK_DIR}"
+
+if test $? -ne 0; then
+ echo 'Make dir error!'; exit 1; fi
+
+SRC_COMMIT='b33d1419282de79e09167f86a03b0f6443624757'
+
+for it in \
+ 'git init' \
+ "git remote add origin https://github.com/${REPOSITORY_OWNER}/${REPOSITORY_NAME}.git" \
+ "git fetch origin ${SRC_COMMIT}" \
+ "git checkout ${SRC_COMMIT}"; do
+ docker exec -w "${WORK_DIR}" "${CONTAINER_NAME}" /usr/local/bin/bash -c "${it}"
+ if test $? -ne 0; then echo 'Checkout error!'; exit 1; fi
+done
+
+for it in \
+ 'gradle clean' \
+ 'gradle lib:assembleUnstableDebug'; do
+ docker exec -w "${WORK_DIR}" "${CONTAINER_NAME}" /usr/local/bin/bash -c "${it}"
+ if test $? -ne 0; then echo 'Gradle error!'; exit 1; fi
+done
+
+#
 
 docker stop "${CONTAINER_NAME}"
 docker rm -f "${CONTAINER_NAME}"
