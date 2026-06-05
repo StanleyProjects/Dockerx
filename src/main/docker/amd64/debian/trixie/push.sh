348c7fdf9c5bb0e193bc9d4e1925ae78556c5001
Stanley Wintergreen <kepocnhh@protonmail.com>
2026-06-05 16:48:24 +0300

    push

--- src/main/docker/amd64/debian/trixie/push.sh
+++ src/main/docker/amd64/debian/trixie/push.sh
@@ -6,18 +6,19 @@ HOST='docker.io'
 NAMESPACE='kepocnhh'
 ISSUER='debian'
 ISSUER_VERSION='trixie'
+ISSUER_PATH="${ARCH}/${ISSUER}/${ISSUER_VERSION}"
 REPOSITORY="${ISSUER}-${ISSUER_VERSION}-${ARCH}"
-IMAGE_VERSION=11
+IMAGE_VERSION='20260518'
 IMAGE_FLAVOR='a'
 IMAGE_TAG="${IMAGE_VERSION}${IMAGE_FLAVOR}"
 IMAGE_NAME="${HOST}/${NAMESPACE}/${REPOSITORY}:${IMAGE_TAG}"
 
 docker build --no-cache \
- -f "${ARCH}/${ISSUER}/${ISSUER_VERSION}/Dockerfile" \
+ -f "src/main/docker/${ISSUER_PATH}/Dockerfile" \
  --platform="${PLATFORM}" -t "${IMAGE_NAME}" .
 
-if test $? -ne 0; then
- echo "Docker build error!"; exit 21; fi
+if [[ $? -ne 0 ]]; then
+ echo 'Docker build error!'; exit 1; fi
 
 CONTAINER_NAME="container.${REPOSITORY}"
 
@@ -27,11 +28,12 @@ docker rm -f "${CONTAINER_NAME}"
 docker run --platform="${PLATFORM}" \
  -id --name "${CONTAINER_NAME}" "${IMAGE_NAME}"
 
-if test $? -ne 0; then
+if [[ $? -ne 0 ]]; then
  echo 'Run error!'; exit 1; fi
 
-docker cp "${ARCH}/debian/${ISSUER_VERSION}/key.pgp" "${CONTAINER_NAME}:/tmp/key.pgp"
-if test $? -ne 0; then echo 'Copy error!'; exit 1; fi
+docker cp 'src/main/res/key.pgp' "${CONTAINER_NAME}:/tmp/key.pgp"
+if [[ $? -ne 0 ]]; then
+ echo 'Copy error!'; exit 1; fi
 
 for it in \
  "test \"\$(cat /etc/flavor)\" == \"${IMAGE_FLAVOR}\"" \
@@ -49,7 +51,8 @@ for it in \
  'file --version' \
  '/usr/local/bin/bash --version'; do
  docker exec "${CONTAINER_NAME}" /usr/local/bin/bash -c "$it"
- if test $? -ne 0; then echo 'Exec error!'; exit 1; fi
+ if [[ $? -ne 0 ]]; then
+  echo 'Exec error!'; exit 1; fi
 done
 
 for it in \
@@ -64,37 +67,41 @@ for it in \
  'GPG_PASSWORD=qwer1234 /usr/local/bin/gpgloopback.sh --output /tmp/key.pgp.sig --detach-sig /tmp/key.pgp' \
  'gpg --verify /tmp/key.pgp.sig /tmp/key.pgp'; do
  docker exec "${CONTAINER_NAME}" /usr/local/bin/bash -c "$it"
- if test $? -ne 0; then echo 'GPG error!'; exit 1; fi
+ if [[ $? -ne 0 ]]; then
+  echo 'GPG error!'; exit 1; fi
 done
 
 docker stop "${CONTAINER_NAME}"
 docker rm -f "${CONTAINER_NAME}"
 
-echo "Push to Docker repository?"
+echo 'Push to Docker repository?'
 read -r YES_OR_NOT
 
-if test "${YES_OR_NOT}" == 'yes'; then
+if [[ "${YES_OR_NOT}" == 'yes' ]]; then
  docker push "${IMAGE_NAME}"
- if test $? -ne 0; then echo 'Push error!'; exit 1; fi
+ if [[ $? -ne 0 ]]; then
+  echo 'Push error!'; exit 1; fi
  echo "Docker image ${IMAGE_NAME} pushed."
 fi
 
-echo "Push to GIT repository?"
+echo 'Push to GIT repository?'
 read -r YES_OR_NOT
 
-if test "${YES_OR_NOT}" == 'yes'; then
+if [[ "${YES_OR_NOT}" == 'yes' ]]; then
  git add . \
   && git commit -m "${REPOSITORY}:${IMAGE_TAG}" \
   && git push
- if test $? -ne 0; then echo 'Commit push error!'; exit 1; fi
+ if [[ $? -ne 0 ]]; then
+  echo 'Commit push error!'; exit 1; fi
 fi
 
 echo "Push tag \"${REPOSITORY}/${IMAGE_TAG}\" to GIT repository?"
 read -r YES_OR_NOT
 
-if test "${YES_OR_NOT}" == 'yes'; then
+if [[ "${YES_OR_NOT}" == 'yes' ]]; then
  git tag "${REPOSITORY}/${IMAGE_TAG}" \
   && git push --tag
- if test $? -ne 0; then echo "Tag \"${REPOSITORY}/${IMAGE_TAG}\" push error!"; exit 1; fi
+ if [[ $? -ne 0 ]]; then
+  echo "Tag \"${REPOSITORY}/${IMAGE_TAG}\" push error!"; exit 1; fi
  git log --graph --all -2
 fi
